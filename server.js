b803a0788f5a4a5f10478e9a15c4bd1d45829592
demola <demolasobaki@gmail.com>
2019-08-12 07:14:16 +0100

    Added connection to database

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+
+//Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
