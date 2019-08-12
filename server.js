907f8fce7f06d0b39ed5668b2820f7f7df7f5af0
demola <demolasobaki@gmail.com>
2019-08-12 07:44:36 +0100

    Created Route files

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
-const app = express();
-const connectDB = require("./config/db");
 
+const connectDB = require("./config/db");
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const app = express();
+const PORT = process.env.PORT || 5000;
 //Connect Database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
 app.get("/", (req, res) => res.send("API Running"));
+
+//Define routes
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
