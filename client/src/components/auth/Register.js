01c0b306aba4f79d76518b7f5bdb90b06195beed
demola <demolasobaki@gmail.com>
2019-09-05 23:56:22 +0100

    Implemented authentication, store token in localstorage, and redirect logged in users in

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
 const Register = props => {
-  const { setAlert, register } = props;
+  const { setAlert, register, isAuthenticated } = props;
 
   const [formData, setFormData] = useState({
     name: "",
@@ -46,6 +46,9 @@ const Register = props => {
     }
   };
 
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
@@ -104,10 +107,15 @@ const Register = props => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { setAlert, register }
 )(Register);
