0755c6ae68f6843cd9b61b8f8fd06389c66f578d
demola <demolasobaki@gmail.com>
2019-09-05 13:14:37 +0100

    Implemented alert reducer, action and components to handle app alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,12 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = props => {
+  const { setAlert } = props;
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,7 +22,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log("SUCCESS");
 
@@ -101,4 +106,11 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { setAlert }
+)(Register);
