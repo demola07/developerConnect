cbc17705da2849ab4397463f0ee6b67c5b7eeea8
demola07 <45898468+demola07@users.noreply.github.com>
2019-12-28 00:41:18 +0100

    Serve static assets in production

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 //Route Paths
-const users = require("./routes/api/users");
-const auth = require("./routes/api/auth");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 //Initialze express bodyparser middleware
 app.use(express.json({ extended: false }));
@@ -16,13 +16,22 @@ app.use(express.json({ extended: false }));
 //Connect Database
 connectDB();
 
-//test route
-app.get("/", (req, res) => res.send("API Running"));
-
 //Define routes and mount them on respective paths
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
